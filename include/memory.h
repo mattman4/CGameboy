8678fc506819c76a7c0ebd8112b2abc0401e4202
mattman4 <91604143+mattman4@users.noreply.github.com>
2025-10-05 14:38:36 +0100

    File select into ROM

--- include/memory.h
+++ include/memory.h
@@ -1,6 +1,8 @@
 #ifndef MEMORY_H_
 #define MEMORY_H_
 
+extern unsigned char rom[0x8000];
+
 unsigned char readMem(unsigned short);
 int writeMem(unsigned short, unsigned char);
 void resetMem(void);
--- src/cart.c
+++ src/cart.c
@@ -1,6 +1,8 @@
 #include <SDL3/SDL_dialog.h>
 #include <SDL3/SDL_log.h>
+#include <SDL3/SDL_iostream.h>
 #include "cart.h"
+#include "memory.h" // for rom
 
 const SDL_DialogFileFilter filters[] = {
     {"GB Files", "gb"}
@@ -17,7 +19,22 @@ void SDLCALL callback(void *userdata, const char* const *filelist, int filter) {
         return;
     }
 
-    SDL_Log("File opened: %s", *filelist);
+    SDL_Log("File selected: %s", *filelist);
+
+    size_t datasize = 0;
+    void *data = SDL_LoadFile(*filelist, &datasize);
+
+    if(!data) {
+        SDL_Log("Error reading file: %s", SDL_GetError());
+        return;
+    }
+
+    if(datasize != 0x8000) {
+        SDL_Log("Error reading file: expected 32k size");
+        return;
+    }
+
+    memcpy(rom, data, 0x8000);
 }
 
 void loadrom(SDL_Window *window) {
